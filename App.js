b230f0a8edb76c818ef9c1a92666aa550b43d09b
Andrew Tam <andrew.tam.000@gmail.com>
2017-09-08 22:12:47 -0400

    Example of using react-native-elements

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Header
+            backgroundColor='gray'
+            leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+            rightComponent={{ icon: 'home', color: '#fff' }}
+        />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
