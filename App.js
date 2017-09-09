da3ae28c22b3e6e486d09ff7395e7aff51af3ae6
Andrew Tam <andrew.tam.000@gmail.com>
2017-09-08 22:32:03 -0400

    Added test component

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from 'react-native-elements';
+import { Tabs, Tab, Icon } from 'react-native-elements';
 
+import Homepage from '~/components/Homepage';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Header
-            backgroundColor='gray'
-            leftComponent={{ icon: 'menu', color: '#fff' }}
-            centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-            rightComponent={{ icon: 'home', color: '#fff' }}
-        />
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <Header
+                    backgroundColor='gray'
+                    leftComponent={{ icon: 'menu', color: '#fff' }}
+                    centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+                    rightComponent={{ icon: 'home', color: '#fff' }}
+                />
+
+                <Homepage/>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
---
+++ components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Homepage = () => (
+    <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+    </View>
+);
+
+export default Homepage;
