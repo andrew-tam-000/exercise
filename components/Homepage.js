dde581dade1ac31660d6f362ae892e46567b73e0
= <jefftam23@gmail.com>
2017-09-09 04:17:37 -0400

    Break out the the default text into a component and also add a component (with a prop) to play around

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { DefaultTextView, CustomizableTextView } from '~/components/index';
+
 const Homepage = () => (
     <View>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <DefaultTextView />
+        <CustomizableTextView text="Howdy"/>
     </View>
 );
 
--- components/index.js
+++ components/index.js
@@ -1,1 +1,18 @@
-// Add compents here
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const DefaultTextView = () => (
+    <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+    </View>
+);
+
+export const CustomizableTextView = ({ text }) => (
+    <View>
+        <Text>
+            { text }
+        </Text>
+    </View>
+);
